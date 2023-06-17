693a3fb8fda76bea358087103ba1676edeb70b88
adika-at <denishadika1@gmail.com>
2023-06-17 16:18:04 -0700

    corected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle
+ * main - Prints Programming is like building a multilingual puzzle
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle);
 	return (0);
 }
 
