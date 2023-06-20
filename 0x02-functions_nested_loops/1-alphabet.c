4414a837c9e74f214aa758a8fb7edcb3d03879e8
adika-at <denishadika1@gmail.com>
2023-06-20 14:54:13 -0700

    alphabetic.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	for (j = 'a'; j <= 'j'; j++ )
+	{
+	 _putchar(j);
+	}
+	_putchar('\n');
+	
+}
