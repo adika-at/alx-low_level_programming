be3e2a00f4a05ab7ee4b6a6e2b1be553e909c9a6
adika-at <denishadika1@gmail.com>
2023-06-20 15:17:32 -0700

    final

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,16 +7,14 @@
  * Return: Always 0.
  */
 
-int main(void)
-
+void print_alphabet(void)
 {
-	char j;
+	char c;
 
-	for (j = 'a'; j <= 'j'; j++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-	 putchar (j);
+		_putchar(c);
 	}
-	 putchar ('\n');
-	 return (0);
 
+	_putchar('\n');
 }
