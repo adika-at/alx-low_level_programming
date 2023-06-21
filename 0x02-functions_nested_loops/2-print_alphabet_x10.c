fe6a3636f8e87fcbd66225a1bcea4d73f892cab7
adika-at <denishadika1@gmail.com>
2023-06-21 12:17:31 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char v;
+    int i = 0;
+
+    while (1 <= 9)
+    {
+    for (v = 'a'; v <= 'z'; v++)
+    {
+	    _putchar(v);
+    }
+
+   _putchar('\n');
+
+}
+    return (0);
+}
