6b13abbf886506528a1c9056105b4ab515b9a932
adika-at <denishadika1@gmail.com>
2023-06-21 12:40:43 -0700

    done it

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * c - this  is an integer representing the lowercase character
+ * Return: Always 0.
+ */
+
+int main(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
